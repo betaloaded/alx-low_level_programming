a8ed9f435a2160b5035eb89c1af19926da56ca57
Abdulmumin Ibrahim <betaloaded@gmail.com>
2023-08-17 23:28:08 +0000

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,13 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n + '0');
-		putchar(',');
-		putchar(' ');
+		if (n == 9)
+			putchar(n + '0');
+		else
+		{
+			putchar(n + '0');
+			putchar(',');
+		}
 	}
 	putchar('\n');
 	return (0);
