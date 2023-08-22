f40f76a67b81c85fb79fadaf16accf45640378bd
Abdulmumin Ibrahim <betaloaded@gmail.com>
2023-08-22 13:57:58 +0000

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
-  * main - entry
+  * _islower -  checks for lowercase character
+  *
+  * Description:  function that checks for lowercase character
   *
   * Return always 0
   */
