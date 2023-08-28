994731b64f14a7f02a8c7a84034cfc12603854b6
Abdulmumin Ibrahim <betaloaded@gmail.com>
2023-08-28 23:04:41 +0000

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 15 == 0)
 		{
 			printf("%s", "FizzBuzz");
 		}
@@ -27,7 +27,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 		if (i != 100)
 		{
