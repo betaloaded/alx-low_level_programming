dff85ae1fc4b4061a6f7554793672dbbb4cce59c
Abdulmumin Ibrahim <betaloaded@gmail.com>
2023-08-31 11:23:38 +0000

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -19,17 +19,17 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-			return (0);
+		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
-   * check_pal - checks the characters recursively for palindrome
-    * @s: string to check
-     * @i: iterator
-      * @len: length of the string
-       * Return: 1 if palindrome, 0 if not
-        */
+  * check_pal - checks the characters recursively for palindrome
+  * @s: string to check
+  * @i: iterator
+  * @len: length of the string
+  * Return: 1 if palindrome, 0 if not
+  */
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,15 +7,15 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-			return (-1);
+		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
 /**
-   * actual_sqrt_recursion - recurses to find the natural square root of a number
-    * @n: number to calculate the square of
-     * @i: iterator
-      * Return: the resulting square root
-       */
+  * actual_sqrt_recursion - recurses to find the natural square root
+  * @n: number to calculate the square of
+  * @i: iterator
+  * Return: the resulting square root
+  */
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
