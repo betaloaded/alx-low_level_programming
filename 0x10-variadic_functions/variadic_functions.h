a471140133030b747b73cf9a9910191f9d2cd66f
Abdulmumin Ibrahim <betaloaded@gmail.com>
2023-09-15 22:26:44 +0000

    0x10-variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
-#ifndef VARIADIC_FUNCTION_H
-#define VARIADIC_FUNCTION_H
+#ifndef _VARIADIC_H
+#define _VARIADIC_H
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
