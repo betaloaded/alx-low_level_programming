57d78dea9b61edb3ba72fff8ce57964433c30cf4
Abdulmumin Ibrahim <betaloaded@gmail.com>
2023-09-18 13:45:24 +0000

    arc agv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,26 +2,41 @@
 #include "main.h"
 /**
  * atoi - program to change string to char
- * @str: string
+ * @s: string
  * Return: 0
  */
-int atoi(char *str)
+int atoi(char *s)
 {
-	int res = 0;
-	int sign = 1;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	if (str[0] == '-')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		sign = -1;
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
-	for (; str[i] != '\0'; i++)
-	{
-		res = res * 10 + str[i] - '0';
-		return (res * sign);
-	}
-	return (0);
+	if (f == 0)
+		return (0);
+	return (n);
 }
 /**
   * main - program to multiply two numbers
@@ -33,16 +48,16 @@ int main(int argc, char *argv[])
 {
 	int z, j;
 
-	if (argc == 3)
-	{
-		z = atoi(argv[1]);
-		j = atoi(argv[2]);
-		printf("%d\n", z * j);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	else
+	{
+		 z = atoi(argv[1]);
+		 j = atoi(argv[2]);
+		 printf("%d\n", z * j);
 	}
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,10 +12,11 @@
 int check_num(char *str)
 {
 	unsigned int count;
+
 	count = 0;
 	while (count < strlen(str)) /*count string*/
 	{
-		if (!isdigit(str[count])) /*check if str there are digit*/		
+		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
 		}
@@ -40,21 +41,21 @@ int main(int argc, char *argv[])
 	int count;
 	int str_to_int;
 	int sum = 0;
+
 	count = 1;
 	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-			sum += str_to_int;																                }
-		/*Condition if one of the number contains symbols that are not digits*/
-							
+			sum += str_to_int;
+		} /*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		count++
+		count++;
 	}
 	printf("%d\n", sum); /*print sum*/
 	return (0);
