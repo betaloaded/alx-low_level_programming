9cda06652262eed3091f7eae8ee886295166a423
betaloaded <gobetaloaded@gmail.com>
2023-10-07 22:29:42 +0000

    argcargv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int _atoi(char *str);
+#include <stdlib.h>
 /**
   * main - function to multiply two numbers
   * @argc: arg counter
@@ -12,30 +12,17 @@ int main(int argc, char *argv[])
 	int num1;
 	int num2;
 
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
-	return (0);
-}
-/**
-  * _atoi - converts a string to an integer
-  * @str: contains str
-  * Return: 0
-  */
-int _atoi(char *str)
-{
-	int i = 0;
-	int res = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
+	else
 	{
-		res = res * 10 + (str[i] - '0');
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
 	}
-	return (res);
+	return (0);
 }
