6427fb308eb4e3fa788b3ff83283f24ac012380b
betaloaded <gobetaloaded@gmail.com>
2023-10-11 22:23:03 +0000

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - program to print name of file
+  *
+  * Return: 0
+  *
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
